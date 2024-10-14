e95d91a40ee2dc51ca0e0d99c36f6cab268972e0
W-L <weilguny.lukas@gmail.com>
2024-10-14 13:06:12 +0200

    add new tests with single region on flowcell

--- tests/playback/test_live_playback.py
+++ tests/playback/test_live_playback.py
@@ -65,10 +65,45 @@ def test_launch_readfish(readfish_toml_loc, playback_dir):
         unb = int(llast[3].split(':')[-1].replace(',', ''))
         assert unb > 100
         assert seq > 100
+        # ratio of roughly around 1 is expected
         assert seq / unb > 0.8
         assert seq / unb < 1.5
         checked[2] = 1
     assert sum(checked) == 3
+
+
+
+def test_launch_readfish_singleregion(readfish_toml_loc_singleregion, playback_dir):
+    # this is useful to have as first test so that we get a channels toml directly from readfish
+    log = boss.live.LiveRun.launch_readfish(toml=readfish_toml_loc_singleregion, device="MS00000", name="runs")
+    time.sleep(2)  # required for file to be registered, otherwise assertion fails
+    assert Path(log).is_file()
+    assert Path(log).stat().st_size > 0
+    assert Path('live_alignments.paf').is_file()
+    assert Path('live_reads.fq').is_file()
+    assert Path(f'{playback_dir}/unblocked_read_ids.txt').is_file()
+    assert Path(f'{playback_dir}/channels.toml').is_file()
+    assert '*' not in playback_dir
+    # remove all reads sequenced so far
+    # otherwise readfish takes forever to start doing things
+    fqs = (Path(playback_dir) / 'fastq_pass').glob('*.fastq.gz')
+    for fq in fqs:
+        fq.unlink()
+    pods = (Path(playback_dir) / 'pod5').glob('*.pod5')
+    for pod in pods:
+        pod.unlink()
+
+    time.sleep(5)
+    subprocess.run('pkill -f readfish', shell=True)
+
+    # check that the channels file contains only one condition
+    # and that boss uses all channels accordingly
+    sequencer = boss.live.LiveRun.connect_sequencer(device="MS00000")
+    sequencer.grab_channels(run_name="runs")
+    channels = sequencer.channels
+    assert isinstance(channels, set)
+    assert len(channels) == 0
+
 
 
 
